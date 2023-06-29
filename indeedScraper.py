f7efa15fe2f64cfb732401835c8b71de2e6e6f61
harleyMcLachlan <60732536+harleyMcLachlan@users.noreply.github.com>
2023-06-28 21:21:06 -0700

    readability and documentation

--- indeedScraper.py
+++ indeedScraper.py
@@ -9,133 +9,138 @@ from selenium.webdriver.support import expected_conditions as EC
 import customtkinter as ctk
 from webdriver_manager.chrome import ChromeDriverManager
 
-##################### JOB FINDING ########################################
+### Main function to populate screen with jobs
+def populateJobs(driver,window):
 
-### closes popups if possible
-def closePopups(driver):
-    try:
-        closes = driver.find_elements(By.CLASS_NAME,"icl-CloseButton")
-        if len(closes) > 0:
-            for close in closes:
-                close.click()
-    except:
-        pass
-    
-    try:
-        closes = driver.find_elements(By.ID,"onetrust-reject-all-handler")
-        if len(closes) > 0:
-            for close in closes:
-                close.click()
-    except:
-        pass
+    #OPENS indeed to engineer jobs in berlin last 24 hours
+    driver.get("https://de.indeed.com/jobs?q=engineer&l=Berlin&fromage=1&vjk=0a4a7664e5720982")
 
-    try:
-        closes = driver.find_elements(By.CSS_SELECTOR,"button[aria-label='schließen']")
-        if len(closes) > 0:
-            for close in closes:
-                close.click()
-    except:
-        pass
-
-    try:
-        closes = driver.find_elements(By.CSS_SELECTOR,"button[aria-label='close']")
-        if len(closes) > 0:
-            for close in closes:
-                close.click()
-    except:
-        pass
-    
+    #Loops through each page
+    while True:
+        getJobs(driver,window)
+        if(debug or not(nextPage(driver))):                                                 #Break if debug mode is on or run out of pages
+            time.sleep(0.5)                                                                 #Give time to load last page. (Crashes without)
+            window.labels[0].configure(text="Results: " + str(window.my_frame.numRows))     #Update the results text
+            driver.close()                                                                  #Close the chrome window
+            break
 
 ###Get list of jobs on page            
 def getJobs(driver,window):
     closePopups(driver)
-    jobList = driver.find_elements(By.XPATH,"/html/body/main/div/div[1]/div/div/div[5]/div[1]/div[5]/div/ul/li")
-    #Get job snippet text
-    for jobListing in jobList:
-        snippets = jobListing.find_elements(By.CLASS_NAME,"job-snippet")
-        for snippet in snippets:
-            #detect language
-            try:
-                if detect(snippet.text) == "en":
-                    #if language is english, print the title
-                    titles = jobListing.find_elements(By.CLASS_NAME,"jobTitle")
-                    for title in titles:
-                        #filter out words
-                        if any(substring.lower() in title.text.lower() for substring in filteredWordList):
-                            continue
-                        links = jobListing.find_elements(By.CLASS_NAME,"jcs-JobTitle")
-                        for link in links:
-                            jobTitle = trimStrLen(title.text,70)
-
-                            #check links for already seen jobs
-                            if any(alreadyOpenedLink in jobTitle for alreadyOpenedLink in alreadySearchedJobsList):
-                                window.my_frame.createBtn(jobTitle,link.get_attribute('href'),True)
-                            else:
-                                window.my_frame.createBtn(jobTitle,link.get_attribute('href'))
-                                file.write(jobTitle + "\n")     
+    jobList = driver.find_elements(By.XPATH,"/html/body/main/div/div[1]/div/div/div[5]/div[1]/div[5]/div/ul/li")    #Gets all jobs elements on page
     
-            except:
-                pass
-                    
+    for jobListing in jobList:                                                                                      #For each job, get snippet text
+        snippet = jobListing.find_elements(By.CLASS_NAME,"job-snippet")                                                                                                       #For each snippet detect the language
+        try:                                                                                                    
+            if detect(snippet[0].text) == "en":                                                                     #Check language of snippet
+                titleElement = jobListing.find_elements(By.CLASS_NAME,"jobTitle")                                   #Get job title element
+                checkTitle(titleElement[0],jobListing,window,file)                                                  #check title for filtered words and already seen jobs
+        except:
+            pass
+
+### check title for filtered words and already seen jobs
+def checkTitle(titleElement,jobListing,window,jobFile):
+    jobTitle = trimMaxStrLen(titleElement.text) #shorten job title if needed
+
+    #check title for filtered words (makes everything lowercase for comparison)
+    if any(filteredWord.lower() in titleElement.text.lower() for filteredWord in filteredWordList):
+        return
+    
+    jobLinkElement = jobListing.find_elements(By.CLASS_NAME,"jcs-JobTitle")                                 #Get job link element
+    
+    #check titles for already seen jobs
+    if any(alreadyOpenedJobTitles in jobTitle for alreadyOpenedJobTitles in alreadySeenJobsList):
+        window.my_frame.createBtn(jobTitle,jobLinkElement[0].get_attribute('href'),True)                    #If already seen, create greyed button
+    else:
+        window.my_frame.createBtn(jobTitle,jobLinkElement[0].get_attribute('href'))                         #If not seen, create blue button
+        jobFile.write(jobTitle + "\n")                                                                      #and write title to the seen jobs list
+    
 ### goes to next page of jobs if possible
 def nextPage(driver):
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    time.sleep(0.2)
-    pages = driver.find_elements(By.CSS_SELECTOR,"a[aria-label='Next Page']")
-    if len(pages) == 0:
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")                                #Scroll to bottom of page                            
+    time.sleep(0.2)                                                                                         #Wait for scroll to complete
+    page = driver.find_elements(By.CSS_SELECTOR,"a[aria-label='Next Page']")                                #Get next page element
+    if len(page) == 0:                                                                                      #Return false if no more next page
         return 0
-    for page in pages:
-        try:
-            page.click()
-        except:
-            time.sleep(0.5)
-            closePopups(driver)
-            page.click()
-        elem=WebDriverWait(driver,5).until(EC.presence_of_element_located((By.CLASS_NAME,"job-snippet")))
+    try:
+        page[0].click()                                                                                     #Otherwise click next page
+    except:
+        time.sleep(0.5)                                                                                     #Sometimes need to wait for popups and try again
+        closePopups(driver)
+        page[0].click()
+
+    elem=WebDriverWait(driver,5).until(EC.presence_of_element_located((By.CLASS_NAME,"job-snippet")))       #Wait for next page to load
     return 1
 
-### Main call to populate screen with jobs
-def populateJobs(driver,window):
-    #OPENS indeed to engineer jobs in berlin last 24 hours
-    driver.get("https://de.indeed.com/jobs?q=engineer&l=Berlin&fromage=1&vjk=0a4a7664e5720982")
+########################## HELPER FUNCTIONS ############################
 
-    getJobs(driver,window)
-    while(not(debug) and nextPage(driver)):
-       getJobs(driver,window)
-    time.sleep(0.5)
-    window.labels[0].configure(text="Results: " + str(window.my_frame.numRows))
-    driver.close()
+### Checks for and closes popups
+def closePopups(driver):
+    #Store all elements with close buttons in array
+    closeButtonElements = []
+    try:
+        closeButtonElements.append(driver.find_elements(By.CLASS_NAME,"icl-CloseButton"))
+    except:
 
-def trimStrLen(str,len=50):
+        pass
+    try:
+        closeButtonElements.append(driver.find_elements(By.ID,"onetrust-reject-all-handler"))
+    except:
+        pass
+
+    try:
+        closeButtonElements.append(driver.find_elements(By.CSS_SELECTOR,"button[aria-label='schließen']"))
+    except:
+        pass
+
+    try:
+        closeButtonElements.append(driver.find_elements(By.CSS_SELECTOR,"button[aria-label='close']"))
+    except:
+        pass
+
+    #Click on all the close buttons
+    for closeButtonElement in closeButtonElements:
+        for closeButton in closeButtonElement:
+            try:
+                closeButton.click()
+            except:
+                pass
+
+### Trim length of job title to fit in window
+def trimMaxStrLen(str,len=70):
     return str[:len]
-
-### callback function for button
-def btnCall(btn,url):
-    openLink(url)
-    btn.configure(fg_color="grey")
-
-def xBtnCall(btn):
-    btn.configure(fg_color="transparent")
 
 ### opens link in default browser
 def openLink(url):
     webbrowser.open(url, new=0, autoraise=False)
 
-#################### GUI ####################
+### Opens job link
+def jobBtnCall_openJobLink(btn,url):
+    openLink(url)
+    btn.configure(fg_color="grey")
+
+### Deletes job from GUI list
+def XBtnCall_deleteJob(btn):
+    btn.configure(fg_color="transparent")
+
+### Submits word to filtered word list
+def entryCall_submitWord(event):
+    file = open("filtered words.txt","a")
+    file.write("\n" + event.widget.get())
+    file.close()
+    wordFrame.createBtn(event)
+
+########################## GUI ############################
 class App(ctk.CTk):
     numResults = 0
     labels = []
     def __init__(self):
         super().__init__()
-
-        self.labels.append(ctk.CTkLabel(master=self,
-                                  font=("Arial",20),
-                                  text="Results: " + str(self.numResults)))
+        self.labels.append(ctk.CTkLabel(master=self, font=("Arial",20), text="Results: " + str(self.numResults)))
         self.labels[0].grid(row=0,column=0,padx=20)
         self.my_frame = MyFrame(master=self, width=1000, height=400)
         self.my_frame.grid(row=1, column=0, padx=20)
         
-
 class MyFrame(ctk.CTkScrollableFrame):
 
     buttons = []
@@ -148,18 +153,18 @@ class MyFrame(ctk.CTkScrollableFrame):
         btnNum = self.numRows
         self.buttons.append(ctk.CTkButton(self,
                 text=title,
-                command= lambda: btnCall(self.buttons[btnNum],url),
+                command= lambda: jobBtnCall_openJobLink(self.buttons[btnNum],url),
                 hover_color="green",
                 font=("Arial",20),
                 width=self.cget("width") - 100,
             ))
+        self.buttons[btnNum].grid(row=self.numRows,column=0,pady=5,padx=10)
         if alreadySeen:
             self.buttons[btnNum].configure(fg_color="#5A5A5A")
-        self.buttons[btnNum].grid(row=self.numRows,column=0,pady=5,padx=10)
 
         self.xbuttons.append(ctk.CTkButton(self,
                 text="X",
-                command= lambda: xBtnCall(self.buttons[btnNum]),
+                command= lambda: XBtnCall_deleteJob(self.buttons[btnNum]),
                 hover_color="red",
                 font=("Arial",20),
                 width=self.cget("width") - self.buttons[0].cget("width") - 10
@@ -195,67 +200,61 @@ class MyWordFrame(ctk.CTkScrollableFrame):
     
     numRows = 0
 
-def submitWord(event):
-    file = open("words.txt","a")
-    file.write("\n" + event.widget.get())
-    file.close()
-    wordFrame.createBtn(event)
+### setup main window and wigdets
+def windowSetup():
+    #set main window properties
+    ctk.set_appearance_mode("System")
+    ctk.set_default_color_theme("blue")
+    window.geometry("1080x750")
 
+    #setup frame to hold word filter related widgets
+    entryFrame = ctk.CTkFrame(master=window)
+    entryFrame.grid(row=2,column=0,padx=20)
+    entryFrame.place(relx=0.02,y=450)
 
+    #setup entry box for filter words
+    entryWord = ctk.CTkEntry(master=entryFrame,
+                        width=300,
+                        font=("Arial",20),
+                        placeholder_text="Enter word to filter out...",
+                        )
+    entryWord.grid(row=0,column=0,padx=20)
+    entryWord.bind('<Return>', entryCall_submitWord)
 
+    #setup frame to hold filter words
+    wordFrame = MyWordFrame(master=entryFrame,
+                                    width=200,
+                                    height=200,
+                                    )
+    wordFrame.grid(row=0,column=2,padx=20,pady=20)
+
+    #load words into the filtered word window
+    for word in filteredWordList:
+        wordFrame.loadWord(word)
 
 ############################ MAIN #############################
+if __name__ == "__main__":
+    debug = False
 
-debug = False
+    #get unwanted word filter list (case doesnt matter)
+    file = open("filtered words.txt","r+")
+    filteredWordList = file.read().splitlines()
+    file.close()
 
+    #open file to read unique already seen job titles"
+    file = open("seen jobs.txt","r+")
+    alreadySeenJobsList = file.read().splitlines()
+    file.close()
+    
+    window = App()
+    windowSetup()
 
-#unwanted word filter list (case doesnt matter)
-file = open("words.txt","r+")
-filteredWordList = file.read().splitlines()
-file.close()
-
-#open file to store unique links"
-file = open("joblinks.txt","r+")
-alreadySearchedJobsList = file.read().splitlines()
-file.close()
-
-
-
-ctk.set_appearance_mode("System")
-ctk.set_default_color_theme("blue")
-window = App()
-window.geometry("1080x750")
-
-#do the job search
-file = open("joblinks.txt","a")
-driver = webdriver.Chrome(ChromeDriverManager().install())
-populateJobs(driver,window)
-file.close()
-
-entryFrame = ctk.CTkFrame(master=window)
-entryFrame.grid(row=2,column=0,padx=20)
-entryFrame.place(relx=0.02,y=450)
-
-entryWord = ctk.CTkEntry(master=entryFrame,
-                     width=300,
-                     font=("Arial",20),
-                     placeholder_text="Enter word to filter out...",
-                     )
-entryWord.grid(row=0,column=0,padx=20)
-
-wordFrame = MyWordFrame(master=entryFrame,
-                                   width=200,
-                                   height=200,
-                                   )
-wordFrame.grid(row=0,column=2,padx=20,pady=20)
-
-
-for word in filteredWordList:
-    wordFrame.loadWord(word)
-
-entryWord.bind('<Return>', submitWord)
-
-window.mainloop()
+    #do the job search
+    file = open("seen jobs.txt","a")
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    populateJobs(driver,window)
+    file.close()
+    window.mainloop()
 
 
 
